ab44a074a86e51fe46e8b859782c416697dd7ad0
Saksham <sakshamjohri@gmail.com>
2021-07-25 16:34:15 +0530

    ADD: backgroud image

---
+++ src/containers/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Main = () => {
+    return (
+        <div className='main-page'>
+            hello
+        </div>
+    )
+}
+
+export default Main
