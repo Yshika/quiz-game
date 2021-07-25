c64a2fc61ce468a45682911ef3c7cb4279a2be19
Yshika Agarwal <yshika.agarwal@geminisolutions.in>
2021-07-25 17:37:56 +0530

    front-page

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './style.scss';
-
+import nerd from "../../assets/images/nerd.png"
 const Main = () => {
     return (
         <div className='main-page'>
-            hello
+            <div className="container">
+                <section className="left">
+                    <p>
+                        OH, Hello There Nerd!
+                        <br />
+                        A Perfect Quiz just for you...
+                        <br />
+                        Just click on the button below
+                        <br />
+                        & Choose your Poison!
+                    </p>
+                    <button className="btn">Take a Quiz</button>
+                </section>
+                <section className="right">
+                    <div className="img-container">
+                        <img src={nerd} alt="" />
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
